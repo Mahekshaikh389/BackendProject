5095e43851170a86173ab3d5b05032c1c50e0d00
Mahekshaikh389 <mahekm389@gmail.com>
2025-12-05 15:10:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ dotenv.config();
 
 const app = express();
 app.use(cors({origin: "https://frontend-z6sf.onrender.com",}));
-app.use(express.json());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://frontend-z6sf.onrender.com"
+  ],
 
 connectDB();
 
